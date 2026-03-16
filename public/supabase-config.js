f0f33c1e17a38956e326c0da141cf1c1daab06e0
Baron17971 <baranat@gmail.com>
2026-03-16 11:05:40 +0200

    Configure Supabase URL and anon key

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -4,8 +4,8 @@
 window.APP_ID = "interactive-map";
 window.SUPABASE_PROJECTS_TABLE = "teacher_projects";
 
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://tbibhrainfpvukafgdmo.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiaWJocmFpbmZwdnVrYWZnZG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NTAxMDksImV4cCI6MjA4OTIyNjEwOX0.JvjCV2I9GPD6vuU-IF3pEYuqxi5vdbGIcJMpZo0WILw";
 
 /*
 window.SUPABASE_URL = "https://YOUR_PROJECT_REF.supabase.co";
